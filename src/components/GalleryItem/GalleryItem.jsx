068d74e367e29b2fb0fe95de37277fd3f1f3be67
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-05 12:11:52 -0500

    like function working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -8,13 +8,13 @@ function GalleryItem ({image, getImageList}) {
     const handleLike = () => {
         console.log('In handleLike');
 
-        newCount = image.likes + 1;
+        const newCount = image.likes + 1;
 
         const data = {
             likes: newCount
         }
 
-        axios.put( `/gallery/${image.id}`, data )
+        axios.put( `/gallery/like/${image.id}`, data )
         .then( response => {
             console.log( response );
 
@@ -37,10 +37,11 @@ function GalleryItem ({image, getImageList}) {
                     {/*
                     if showDescription is true, show image
                     */}
-                    <img onClick={() => setShowDescription(!showDescription)} key={image.id} src={image.path}></img>
-                    <button key={image.id}>Like</button>
+                    <img onClick={() => setShowDescription(!showDescription)} src={image.path}></img>
+                <section>
+                    <button onClick={handleLike} key={image.id}>Like</button>
                     <p>Likes: {image.likes}</p>
-
+                </section>
                 </div>
             )}
            
